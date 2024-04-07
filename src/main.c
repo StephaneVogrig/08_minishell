19597122342d4efc55db3a724c2f20d07ef03969
Stephanie Mortemousque <smortemo@3D3.42angouleme.fr>
2024-04-07 22:00:57 +0200

    add to_do_list + Main: fix exit when ctrl-C

--- src/main.c
+++ src/main.c
@@ -3,40 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
+/*   By: smortemo <smortemo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 16:55:55 by svogrig           #+#    #+#             */
-/*   Updated: 2024/04/06 22:03:38 by svogrig          ###   ########.fr       */
+/*   Updated: 2024/04/07 21:28:52 by smortemo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
 #include "exec_input.h"
+#include "minishell.h"
 
 int	main(int argc, char **argv, char **envp)
 {
 	char	*input;
 
-	if (argc || argv){};	
+	if (argc || argv)
+	{
+	};
 	signal(SIGINT, handler_ctrl_c);
 	signal(SIGQUIT, SIG_IGN);
 	while (1)
 	{
 		input = readline(PROMPT_MINISHELL);
 		if (!input)
-			break;
+		{
+			write(1, "exit\n", 5);
+			break ;
+		}
 		if (ft_strncmp("exit", input, 4) == 0)
 		{
 			free(input);
 			write(1, "exit\n", 5);
-			break;
+			break ;
 		}
 		if (*input)
 		{
 			add_history(input);
-			exec_input(input, envp);	
+			exec_input(input, envp);
 		}
 		free(input);
-	}		
+	}
 	return (EXIT_SUCCESS);
 }
