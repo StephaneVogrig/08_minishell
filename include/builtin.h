1638149f93aea29bd1999b581e7eb4cdf98b10d4
Stephanie Mortemousque <smortemo@3C4.42angouleme.fr>
2024-04-29 22:51:33 +0200

    unset for linked list in progress

--- include/builtin.h
+++ include/builtin.h
@@ -6,7 +6,7 @@
 /*   By: smortemo <smortemo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 15:37:55 by smortemo          #+#    #+#             */
-/*   Updated: 2024/04/18 22:03:25 by smortemo         ###   ########.fr       */
+/*   Updated: 2024/04/29 22:48:35 by smortemo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@
 # include "environment.h"
 # include "libft.h"
 
-int	    ft_strtab_size(char **strtab); // ajouter dans libft ?
-char	**builtin_unset(char **envp, char *str);
+int	ft_strtab_size(char **strtab); // ajouter dans libft ?
+// char	**builtin_unset(char **envp, char *str);
 char	**builtin_export(char **envp, char *str);
 void	export_alone(char **envp);
-int 	builtin_env(char **envp);
+int		builtin_env(char **envp);
+
+int		builtin_unset(t_env *env, char *str);
 
 #endif
--- src/builtin_unset.c
+++ src/builtin_unset.c
@@ -6,47 +6,74 @@
 /*   By: smortemo <smortemo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 15:37:23 by smortemo          #+#    #+#             */
-/*   Updated: 2024/04/12 00:00:58 by smortemo         ###   ########.fr       */
+/*   Updated: 2024/04/29 22:48:06 by smortemo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
 // ajouter dans libft ?
-int	ft_strtab_size(char **strtab)
-{
-	int	i;
+// int	ft_strtab_size(char **strtab)
+// {
+// 	int	i;
 
-	if (!strtab)
+// 	if (!strtab)
+// 		return (0);
+// 	i = 0;
+// 	while (*strtab++)
+// 		i++;
+// 	return (i);
+// }
+
+// char	**builtin_unset(char **envp, char *str)
+// {
+// 	int		i;
+// 	int		j;
+// 	int		len;
+// 	char	**tab_temp;
+
+// 	if (!env_get(envp, str))
+// 		return (envp);
+// 	j = 0;
+// 	i = 0;
+// 	len = ft_strlen(str);
+// 	tab_temp = mem_calloc(ft_strtab_size(envp), sizeof(char **));
+// 	while (envp[j])
+// 	{
+// 		if (ft_strncmp(envp[j], str, len) != 0)
+// 			tab_temp[i++] = ft_strdup(envp[j]);
+// 		if (ft_strncmp(envp[j], str, len) == 0 && envp[j][len] != '=')
+// 			tab_temp[i++] = ft_strdup(envp[j]);
+// 		j++;
+// 	}
+// 	strtab_free(envp);
+// 	envp = tab_temp;
+// 	return (envp);
+// }
+
+int	builtin_unset(t_env *env, char *str)
+{
+	t_env *temp;
+
+	if (!env || !str)
 		return (0);
-	i = 0;
-	while (*strtab++)
-		i++;
-	return (i);
-}
-
-char	**builtin_unset(char **envp, char *str)
-{
-	int		i;
-	int		j;
-	int		len;
-	char	**tab_temp;
-
-	if (!env_get(envp, str))
-		return (envp);
-	j = 0;
-	i = 0;
-	len = ft_strlen(str);
-	tab_temp = mem_calloc(ft_strtab_size(envp), sizeof(char **));
-	while (envp[j])
+	while (env)
 	{
-		if (ft_strncmp(envp[j], str, len) != 0)
-			tab_temp[i++] = ft_strdup(envp[j]);
-		if (ft_strncmp(envp[j], str, len) == 0 && envp[j][len] != '=')
-			tab_temp[i++] = ft_strdup(envp[j]);
-		j++;
+		if (ft_strcmp(env->name, str))
+		{
+			free(env->name);
+			free(env->value);
+			temp = env;
+			while (temp)
+			{
+				temp = env->next;
+				env = temp;
+			}
+			free(temp);
+			env->next = NULL;
+			return (0);
+		}
+		env = env->next;
 	}
-	strtab_free(envp);
-	envp = tab_temp;
-	return (envp);
+	return (0);
 }
