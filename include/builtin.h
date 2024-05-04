56e0507b956f8b7300133acfbed967107283546e
Stephanie Mortemousque <smortemo@3D3.42angouleme.fr>
2024-05-04 15:05:56 +0200

    donne CD + EXIT + return error value builtin

--- include/builtin.h
+++ include/builtin.h
@@ -6,7 +6,7 @@
 /*   By: smortemo <smortemo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 15:37:55 by smortemo          #+#    #+#             */
-/*   Updated: 2024/05/04 00:57:32 by smortemo         ###   ########.fr       */
+/*   Updated: 2024/05/04 01:32:21 by smortemo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ int		builtin_echo(t_cmd *cmd, t_env *env);
 int		builtin_cd(t_cmd *cmd, t_env *env);
 int		builtin_exit(t_cmd *cmd, t_env *env);
 
-int		cd_for_test(t_env *env, char *str);
+// int		cd_for_test(t_env *env, char *str);
 
 // int	builtin_export(t_env *envp, char *str);
 // int		builtin_env(char **envp);
