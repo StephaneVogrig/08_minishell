479d6df8d2102d634a849d7c62f38183541ae199
Stephanie Mortemousque <smortemo@3D3.42angouleme.fr>
2024-05-19 15:40:23 +0200

    export bug fix + malloc protected

--- src/builtins/builtin_export.c
+++ src/builtins/builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: smortemo <smortemo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 15:13:40 by smortemo          #+#    #+#             */
-/*   Updated: 2024/05/18 18:07:41 by smortemo         ###   ########.fr       */
+/*   Updated: 2024/05/19 14:32:19 by smortemo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,10 +48,6 @@ int	end_var_name(char *str)
 	return (i);
 }
 
-// les 2 fct suivantes sont dans manage_node.c (+declaration environment.h)
-// t_bool	var_init(t_env *node, char *str, int n) 
-// int	export_new_node(t_env *env, char *str, int n)
-
 static int	export_run(t_env *env, char *str)
 {
 	int		n;
@@ -66,7 +62,11 @@ static int	export_run(t_env *env, char *str)
 		node->value = ft_strdup(&str[n + 1]);
 	}
 	if (node && str[n] == '+')
+	{
 		node->value = ft_strjoin_free_s1(node->value, &str[n + 2]);
+		if (!node->value)
+			return (ENOMEM);
+	}
 	if (!node)
 		return (export_new_node(env, str, n));
 	return (0);
--- src/builtins/builtin_export_alone.c
+++ src/builtins/builtin_export_alone.c
@@ -6,59 +6,92 @@
 /*   By: smortemo <smortemo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 22:22:07 by smortemo          #+#    #+#             */
-/*   Updated: 2024/05/18 18:33:15 by smortemo         ###   ########.fr       */
+/*   Updated: 2024/05/19 15:32:42 by smortemo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
-void	display_tab_export(char **tab, int i)
+void	env_sorted_display(t_env *env)
 {
-	while (tab[i])
+	while (env != NULL)
 	{
-		printf("declare -x %s", tab[i]);
-		free(tab[i]);
-		i++;
-		if (tab[i] != NULL)
-		{
-			printf("\"%s\"", tab[i]);
-		}
-			
-		free(tab[i]);
-		printf("\n");
-		i++;
+		if(env->type == EXPORTED)
+			printf("declare -x %s=\"%s\"\n", env->name, env->value);
+		else
+			printf("declare -x %s\n", env->name);
+		env = env->next;
 	}
-	free(tab);
-	return ;
 }
 
-void	modify_tab(char **sorted_tab)
+t_bool	node_init_v2(t_env *node, char *str)///possible de modifier node_init ?
 {
-	char	**tab_to_display;
-	int		i;
-	int		j;
-	int		count;
+	int	index;
 
-	tab_to_display = (char **)malloc(sizeof(char *) * (2
-				* ft_strtab_size(sorted_tab) + 1));
-	j = 0;
-	i = 0;
-	while (sorted_tab[j])
+	if (ft_strchr(str, '='))
 	{
-		count = ft_strchr_i(sorted_tab[j], '=');
-		if (count != 0)
-		{
-			tab_to_display[i++] = ft_strndup(sorted_tab[j], count + 1);
-			tab_to_display[i++] = ft_strdup(&sorted_tab[j++][count + 1]);
-		}
-		else
-		{
-			tab_to_display[i++] = ft_strdup(sorted_tab[j++]);
-			tab_to_display[i++] = NULL;
-		}
+		index = ft_strchr_i(str, '=');
+		node->value = ft_strdup(&str[index + 1]);
+		if (!node->value)
+			return (FAILURE);
+		node->type = EXPORTED;
 	}
-	tab_to_display[i] = NULL;
-	display_tab_export(tab_to_display, 0);
+	else
+	{
+		index = ft_strlen(str);
+		node->value = malloc(1);
+		if (!node->value)
+			return (FAILURE);
+		node->type = INTERNAL;
+	}
+	node->name = ft_strndup(str, index);
+	if (!node->name)
+		return (FAILURE);
+	node->next = NULL;
+	return (SUCCESS);
+}
+
+t_env	*envp_to_env_v2(char **envp)///possible de modifier envp_to_env ?
+{
+	t_env	*env;
+	t_env	*node;
+
+	if (!envp)
+		return (NULL);
+	node = NULL;
+	env = NULL;
+	while (*envp)
+	{
+		node = malloc(sizeof(*node));
+		if (!node)
+		{
+			env_free(env);
+			return (NULL);
+		}
+		if (node_init_v2(node, *envp) == FAILURE)
+		{
+			env_free(env);
+			return (NULL);
+		}
+		env_add_back(&env, node);
+		envp++;
+	}
+	return (env);
+}
+
+int	modify_tab(char **sorted_tab)
+{
+	t_env	*sorted_env;
+	t_env	*node;
+	
+	node = NULL;
+	sorted_env = NULL;
+	sorted_env = envp_to_env_v2(sorted_tab);
+	if (!sorted_env)
+		return (ENOMEM);
+	env_sorted_display(sorted_env);
+	env_free(sorted_env);
+	return (0);
 }
 
 void	strtab_sort(char **to_sort)
@@ -93,10 +126,10 @@ char	**env_to_envp_export(t_env *env, int size)
 	int		i;
 
 	i = 0;
-	envp = calloc(size + 1, sizeof(*envp));
+	envp = calloc(size + 2, sizeof(*envp));
 	if (!envp)
 		return (NULL);
-	while (i < size)
+	while (i <= size)
 	{
 		if (env->name[0] == '_' && env->name[1] == '\0')
 			env = env->next;
