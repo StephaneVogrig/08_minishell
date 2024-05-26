06e6000f4f91b354cae165ab81d87b675fdab80b
Stephane Vogrig <svogrig@3C2.42angouleme.fr>
2024-05-26 21:08:48 +0200

    fix leak fd in case of redirection failure when only one builtin to execute

--- src/execution/exec_builtin.c
+++ src/execution/exec_builtin.c
@@ -6,7 +6,7 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 19:14:20 by smortemo          #+#    #+#             */
-/*   Updated: 2024/05/23 15:11:11 by svogrig          ###   ########.fr       */
+/*   Updated: 2024/05/26 19:01:13 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,11 @@ int	exec_builtin_alone(t_builtin builtin, t_cmd *cmd, t_env *env)
 	fd[0] = dup(0);
 	fd[1] = dup(1);
 	if (exec_redir(cmd->redir) == FAILURE)
-		exit_code = EXIT_FAILURE;
+	{
+		close(fd[0]);
+		close(fd[1]);
+		return (EXIT_FAILURE);
+	}
 	exit_code = builtin(cmd, env);
 	dup2(fd[0], 0);
 	dup2(fd[1], 1);
