49ab11d1c7b5f3b64c64e4590c66d81bbc24e6d3
Stephane Vogrig <svogrig@3C4.42angouleme.fr>
2024-06-03 20:58:09 +0200

    norm last commit

--- src/execution/process.c
+++ src/execution/process.c
@@ -6,7 +6,7 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 04:15:23 by svogrig           #+#    #+#             */
-/*   Updated: 2024/06/03 20:38:53 by svogrig          ###   ########.fr       */
+/*   Updated: 2024/06/03 20:55:35 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,33 +30,48 @@ void	exec_cmd_pipe(t_cmd *cmd, t_env **env)
 	exit(exit_code);
 }
 
+void	handle_pipe_child(t_bool need_pipeout, int *fd_in, int pipe_out[2], \
+							t_cmd *cmd)
+{
+	if (cmd->previous != NULL)
+	{
+		dup2(*fd_in, STDIN_FD);
+		close(*fd_in);
+	}
+	if (need_pipeout)
+	{
+		close(pipe_out[READ]);
+		dup2(pipe_out[WRITE], STDOUT_FD);
+		close(pipe_out[WRITE]);
+	}
+}
 
+void	handle_pipe_parent(t_bool need_pipeout, int *fd_in, int pipe_out[2], \
+							t_cmd *cmd)
+{
+	if (cmd->previous != NULL)
+		close(*fd_in);
+	if (need_pipeout)
+	{
+		close(pipe_out[WRITE]);
+		*fd_in = pipe_out[READ];
+	}
+}
 
 int	process(t_cmd *cmd, int *fd_in, t_env **env)
 {
-	int	pid;
-	int	pipe_out[2];
+	int		pid;
+	int		pipe_out[2];
 	t_bool	need_pipeout;
 
 	need_pipeout = cmd->next && cmd->next->flag == PIPE;
 	if (need_pipeout)
 		if (pipe_ms(pipe_out))
 			return (-1);
-
 	pid = fork();
 	if (pid == 0)
 	{
-		if (cmd->previous != NULL)
-		{
-			dup2(*fd_in, STDIN_FD);
-			close(*fd_in);
-		}
-		if (need_pipeout)
-		{
-			close(pipe_out[READ]);
-			dup2(pipe_out[WRITE], STDOUT_FD);
-			close(pipe_out[WRITE]);
-		}
+		handle_pipe_child(need_pipeout, fd_in, pipe_out, cmd);
 		exec_cmd_pipe(cmd, env);
 	}
 	if (pid == -1)
@@ -65,12 +80,6 @@ int	process(t_cmd *cmd, int *fd_in, t_env **env)
 		if (need_pipeout)
 			close(pipe_out[READ]);
 	}
-	if (cmd->previous != NULL)
-		close(*fd_in);
-	if (need_pipeout)
-	{
-		close(pipe_out[WRITE]);
-		*fd_in = pipe_out[READ];
-	}
+	handle_pipe_parent(need_pipeout, fd_in, pipe_out, cmd);
 	return (pid);
 }
