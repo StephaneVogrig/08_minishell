34ed6b0c17687440211740c83ef75b41c333bdf2
Stephane Vogrig <svogrig@3D1.42angouleme.fr>
2024-06-08 22:46:16 +0200

    add mini_strtol.h

--- include/utils/mini_strtol.h
+++ include/utils/mini_strtol.h
@@ -1,24 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtin_exit_utils.h                               :+:      :+:    :+:   */
+/*   mini_strtol.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/23 17:28:18 by svogrig           #+#    #+#             */
-/*   Updated: 2024/06/08 22:37:49 by svogrig          ###   ########.fr       */
+/*   Created: 2024/06/08 22:02:18 by svogrig           #+#    #+#             */
+/*   Updated: 2024/06/08 22:03:13 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BUILTIN_EXIT_UTILS_H
-# define BUILTIN_EXIT_UTILS_H
+#ifndef MINI_STRTOL_H
+# define MINI_STRTOL_H
 
-# include "command.h"
-# include "environment.h"
-# include "mini_strtol.h"
-
-t_bool	is_in_pipe(t_cmd *cmd);
-void	print_exit(t_cmd *cmd, t_env *env);
-t_bool	convert_arg(t_list *argv, int *exit_code);
+long	mini_strtol(const char *nptr, char **endptr);
 
 #endif
--- include/utils/str.h
+++ include/utils/str.h
@@ -6,7 +6,7 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 02:50:36 by svogrig           #+#    #+#             */
-/*   Updated: 2024/05/23 11:14:19 by svogrig          ###   ########.fr       */
+/*   Updated: 2024/06/08 22:38:10 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,5 @@ t_bool		strlist_add_str(t_list **strlist, char *str);
 char		**strlist_to_strtab(t_list *strlist);
 t_char_m	*mini_strndup(char *str, int n);
 t_char_m	*mini_strdup(char *str);
-long		mini_strtol(const char *nptr, char **endptr);
 
 #endif
