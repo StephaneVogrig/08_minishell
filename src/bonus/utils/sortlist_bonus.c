fa8fc88f5375e84d5bf2ad0f5f425c29c5422808
Stephane Vogrig <svogrig@2A2.42angouleme.fr>
2024-06-19 16:40:16 +0200

    add sortlist for wildcard

--- src/bonus/utils/sortlist_bonus.c
+++ src/bonus/utils/sortlist_bonus.c
@@ -1,16 +1,57 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sortlist.c                                         :+:      :+:    :+:   */
+/*   sortlist_bonus.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 17:11:00 by stephane          #+#    #+#             */
-/*   Updated: 2024/06/18 17:41:37 by svogrig          ###   ########.fr       */
+/*   Updated: 2024/06/19 16:37:57 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sortlist_bonus.h"
+
+int	mini_strcmp(char *s1, char *s2)
+{
+	char	c1;
+	char	c2;
+	int		order_noalphanum;
+	int		order_alphanum;
+
+	order_noalphanum = 0;
+	order_alphanum = 0;
+	while (*s1)
+	{
+		c1 = ft_tolower(*s1);
+		c2 = ft_tolower(*s2);
+		if (c1 != c2)
+			return (c1 - c2);
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+
+}
+#include <string.h>
+void	insert(t_list *elem, t_list **strlist)
+{
+	t_list	*previous;
+	t_list	*current;
+
+	previous = NULL;
+	current = *strlist;
+	while (current && mini_strcmp(current->content, elem->content) <= 0)
+	{
+		previous = current;
+		current = current->next;
+	}
+	elem->next = current;
+	if (previous)
+		previous->next = elem;
+	else
+		*strlist = elem;
+}
 
 t_list	*pop(t_list **strlist)
 {
@@ -21,25 +62,6 @@ t_list	*pop(t_list **strlist)
 	temp = *strlist;
 	*strlist = temp->next;
 	return (temp);
-}
-
-void	insert(t_list *elem, t_list **strlist)
-{
-	t_list	*previous;
-	t_list	*current;
-
-	previous = NULL;
-	current = *strlist;
-	while (current && ft_strcmp(current->content, elem->content) <= 0)
-	{
-		previous = current;
-		current = current->next;
-	}
-	elem->next = current;
-	if (previous)
-		previous->next = elem;
-	else
-		*strlist = elem;
 }
 
 void	sort_strlist(t_list **strlist)
